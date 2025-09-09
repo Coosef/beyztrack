e2d3f85cc4a44a5e3d39d77f3e364519ee61254e
BeyzTrack Developer <developer@beyztrack.com>
2025-09-09 17:19:19 +0300

    Fix: Router import hatası düzeltildi - named import yerine default import

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import publicMixin from "./mixins/public";
 import socket from "./mixins/socket";
 import theme from "./mixins/theme";
 import lang from "./mixins/lang";
-import { router } from "./router";
+import router from "./router";
 import { appName } from "./util.ts";
 import dayjs from "dayjs";
 import timezone from "./modules/dayjs/plugin/timezone";
